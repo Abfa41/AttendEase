811e1438e836d283ea264db0e653ac50c425f2b4
ganji <ganjipraveen444@gmail.com>
2024-06-24 12:23:40 +0530

    Ui Updated

--- app/src/main/java/com/example/attendease/MainActivity.java
+++ app/src/main/java/com/example/attendease/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.attendease;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,20 +15,21 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private Button b;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
         b = findViewById(R.id.stutBut);
-        // This is for testing only. delete it afterwards.
-        Intent intent = new Intent(MainActivity.this, qrcode_activity.class);
-        startActivity(intent);
+        imageView = findViewById(R.id.logoImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, qrcode_activity.class));
+            }
+        });
+
     }
 }
